ed7871101930923f4dc4d4f24f7efa30cd71feab
nina-tuyen tran <ninatuyentran@gmail.com>
2023-10-15 15:01:05 -0700

    Fixing whitenoise loading in dnjango 4.2

--- mappybackend/mappybackend/settings.py
+++ mappybackend/mappybackend/settings.py
@@ -124,9 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
