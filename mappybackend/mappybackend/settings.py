e8d4f98f95f73b1af58586525be44df315750ee5
nina-tuyen tran <ninatuyentran@gmail.com>
2023-10-16 10:51:08 -0700

    pushing to render

--- mappybackend/mappybackend/settings.py
+++ mappybackend/mappybackend/settings.py
@@ -18,15 +18,18 @@ import dj_database_url
 env = environ.Env()
 environ.Env.read_env()
 
-...# Your secret key
-SECRET_KEY = env("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = []
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+SECRET_KEY = os.environ.get('django-insecure-728k0bs%91o$^sp%aa_ji@2fmtwpdk7r1na#*$%l2+%)7tnpo3')
+
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1').split(",")
+
+
+
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('mappy-backend.onrender.com')
 if RENDER_EXTERNAL_HOSTNAME:    
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
@@ -79,19 +82,29 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mappybackend.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
+if not DEBUG:
+    DATABASES = {
+	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+    
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# else: 
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': env("DB_NAME"),
+#             'USER': env("DB_USER"),
+#             'PASSWORD': env("DB_PASSWORD"),
+#             'HOST': env("DB_HOST"),
+#             'PORT': env("DB_PORT"),
+#         }
+#     }
 
 
 # Password validation
