0e76f89b7d44ae33e6c7c7e7e0d4f9489cae2543
nina-tuyen tran <ninatuyentran@gmail.com>
2023-10-16 12:35:54 -0700

    bringing dev server back

--- mappybackend/mappybackend/settings.py
+++ mappybackend/mappybackend/settings.py
@@ -86,24 +86,24 @@ if not DEBUG:
 	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
     
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# else: 
+# else:
 #     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': env("DB_NAME"),
-#             'USER': env("DB_USER"),
-#             'PASSWORD': env("DB_PASSWORD"),
-#             'HOST': env("DB_HOST"),
-#             'PORT': env("DB_PORT"),
-#         }
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
+# }
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': env("DB_NAME"),
+            'USER': env("DB_USER"),
+            'PASSWORD': env("DB_PASSWORD"),
+            'HOST': env("DB_HOST"),
+            'PORT': env("DB_PORT"),
+        }
+    }
 
 
 # Password validation
